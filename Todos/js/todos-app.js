08e28d7f93fadfc3bfd9a0ee3f7ea694e1973c59
joseph muganga <jmuganga54@users.noreply.github.com>
2023-03-22 11:42:30 +0300

    Todo app code refactor

--- Todos/js/todos-app.js
+++ Todos/js/todos-app.js
@@ -35,37 +35,7 @@ const filterTodos = {
   hideCompleted: false,
 };
 
-//Function
-const renderTodos = (todos, filter) => {
-  const incompletedTodosArray = todos.filter((todo) => {
-    return !todo.completed;
-  });
 
-  const headingEl = document.createElement("h2");
-  divEl.innerHTML = "";
-
-  incompletedTodosArray.length === 1
-    ? (headingEl.textContent = `You have ${incompletedTodosArray.length} todo which is incompleted`)
-    : (headingEl.textContent = `You have ${incompletedTodosArray.length} todos which are incompleted`);
-
-  divEl.appendChild(headingEl);
-
-  const filterTodoArray = todos.filter((todo) => {
-    let matchedTodo = todo.text
-      .toLowerCase()
-      .includes(filter.searchText.toLowerCase());
-    let hideCompletedMatched = !todo.completed || !filter.hideCompleted;
-    return matchedTodo && hideCompletedMatched;
-  });
-
-  console.log(filterTodoArray);
-  searchTodo.innerHTML = "";
-  filterTodoArray.forEach((todo) => {
-    const paraEl = document.createElement("p");
-    paraEl.textContent = todo.text;
-    searchTodo.appendChild(paraEl);
-  });
-};
 
 //addEventListener
 todoSearched.addEventListener("input", (e) => {
